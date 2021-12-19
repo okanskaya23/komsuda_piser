551bd18eacb9e81828807a1b965d9b7f836462bc
UtkuGursoy <80215921+UtkuGursoy@users.noreply.github.com>
2021-12-19 18:34:03 +0300

    Rating Users
    User rating UI is implemented to Mail.dart

--- lib/Pages/Mail/Mail.dart
+++ lib/Pages/Mail/Mail.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:komsuda_piser_local/Utils/Widgets/app_button.dart';
@@ -8,6 +9,8 @@ import 'package:kartal/kartal.dart';
 import 'package:komsuda_piser_local/Utils/app_textStyles.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+
 
 //TODO burda orderlamayı deneyince bi infinite loop oluyor onabi ara bakalım birde burda tab yapaısı yapsak tıklayınca senin verdiğin siparişler ve sana gelen siparaişler arsında geçiş yapalım
 var TC = "Email_Teyze";
@@ -33,9 +36,12 @@ class Mail extends State<mail> {
 
   }
   final db = FirebaseFirestore.instance;
+  double rating = 0.0; //BU GLOBAL VARIABLE OLMAYACAK, DATABASE'DEN GELECEK
 
   @override
   Widget build(BuildContext context) {
+
+
     return Scaffold(
       appBar: AppBar(
         title: AppButton(
@@ -56,9 +62,34 @@ class Mail extends State<mail> {
           } else
             return ListView(
               children: snapshot.data.docs.map((doc) {
-                return Card(
-                  child: ListTile(
-                    title: Text(doc.data().toString()),
+                return Container(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(doc.get("Email_Teyze").toString()), // EMAIL TEYZE YERINE KULLANICI ADI GELEBİLİR Mİ?
+                      //Text(doc.get("Cost").toString()),
+                      Container(
+                        child: RatingBar.builder(
+                          minRating: 1,
+                          updateOnDrag: true,
+                          itemSize: 20,
+                          itemPadding: EdgeInsets.symmetric(horizontal: 2),
+                          itemBuilder: (context, _) => Icon(Icons.star, color: Colors.amber,),
+                          onRatingUpdate: (rating) => setState(() {
+                            this.rating = rating;
+                            //TODO: BACKEND KISMI BURAYA
+                          }),
+                        ),
+                      ),
+                      AppButton(
+                        text: "Rate",
+                        onPressed: () {},
+                      ),
+                      /*Text(
+                        "Rate user: ${rating}",
+                        style: TextStyle(fontSize: 15),
+                      ),*/
+                    ],
                   ),
                 );
               }).toList(),
